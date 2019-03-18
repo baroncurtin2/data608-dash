a1e9cc5dbd134eba4fb0abb442e4c6783a3cb815
baroncurtin-intentmedia <baron.curtin@intent.com>
2019-03-18 16:50:21 -0400

    Minor updates

--- app.py
+++ app.py
@@ -2,18 +2,37 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-from .nyc_opendata import get_data
+from nyc_opendata import OpenData
 
-
-# obtain df of trees data
-df = get_data()
+# data source
+nyc = OpenData()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='New York City Tree Census')
+    html.H1(children='New York City Tree Census'),
+
+    html.Div(children="Tree Health by Borough"),
+
+    html.Div(children=[
+        html.Label('Borough'),
+        dcc.Dropdown(options=nyc['borough'],
+            value=nyc['borough'][0]),
+
+        html.Label('Health'),
+        dcc.Dropdown(options=nyc['health'],
+                     value=nyc['health'][0]),
+
+        html.Label('Species'),
+        dcc.Dropdown(options=nyc['species'],
+                     value=nyc['species'][0]),
+
+        html.Label('Steward'),
+        dcc.Dropdown(options=nyc['steward'],
+                     value=nyc['steward'][0]),
+    ], style={'columnCount': 2})
 ])
 
 if __name__ == '__main__':
